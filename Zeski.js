76a5762fc3353ba04ecf363d48d1ddeb30213150
Tirth-esh <tirthesh10.aggarwal@gmail.com>
2024-03-06 19:37:37 +0530

    Add Zeski code

---
+++ Zeski.js
@@ -0,0 +1,24 @@
+// const select = document.querySelector('.select');
+// const caret = document.querySelector('.caret');
+// const menu = document.querySelector('.menu');
+// const options = document.querySelectorAll('.menu li');
+// const selected = document.querySelector('.selected');
+
+
+// select.addEventListener('click', () =>{
+//     select.classList.toggle('select-clicked')
+//     menu.classList.toggle('menu-open');
+// })
+
+
+// options.forEach(option => {
+//     option.addEventListener('click', () => {
+//         selected.innerText = option.innerText;
+//         select.classList.remove('select-clicked');
+//         menu.classList.remove('menu-open');
+//         options.forEach(option => {
+//             option.classList.remove('active');
+//         })
+//         option.classList.add('active');
+//     })
+// });
